67d9952c013bae6d9bdb99e004b081f152a32a87
Han Manjong <han@manjong.org>
2017-11-24 21:05:34 +0900

    initial commit

---
+++ Droid/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+
+
+namespace Meme9gag.Droid
+{
+    [Activity(Label = "Meme9gag.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+
+            LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ Meme9gag/Meme9gagPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Xamarin.Forms;
+using Plugin.Media;
+
+namespace Meme9gag
+{
+    public partial class Meme9gagPage : ContentPage
+    {
+        public Meme9gagPage()
+        {
+            InitializeComponent();
+        }
+
+        async void TakePhoto_Clicked(object sender, System.EventArgs e)
+        {
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("오류", "카메라를 사용할 수 없습니다", "확인");
+                return;
+            }
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "Sample",
+                Name = "test.jpg"
+            });
+            if (file == null)
+                return;
+
+            image.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
+        }
+
+        async void PickPhoto_Clicked(object sender, System.EventArgs e)
+        {
+            var file = await CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions
+            {
+            });
+            if (file == null)
+                return;
+            image.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
+        }
+    }
+}
